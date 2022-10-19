b3d637bbfc7f68b23a7309cf78935174067619c3
2011140062akimoto <2011140062m@ed.fuk.kindai.ac.jp>
2022-10-19 06:45:08 +0000

    update: add like&unlike&liled method

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,16 @@ class Tweet < ApplicationRecord
   has_many :like_users, through: :likes, source: :user
   
   validates :user_id, presence: true
+  
+  def like(user)
+    likes.create(user_id: user.id)
+  end
+  
+  def unlike(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+  
+  def liked?(user)
+    like_users.include?(user)
+  end
 end
